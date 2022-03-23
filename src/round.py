7d0b3c2a82eb8df5dd245aea650b73b2eee69dff
Kisu Yang <willow4@korea.ac.kr>
2022-03-23 18:06:22 +0900

    Create round.py

---
+++ src/round.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+
+def round(x:Union[int, float], ndigits:int=None):
+  a = -x if x < 0 else x # absolute
+  i = 0 if ndigits is None else ndigits
+  A = a * 10 ** i
+  A = int(A) if A - int(A) < 0.5 else int(A) + 1 # round
+  a = A / 10 ** i
+  y = -a if x < 0 else a
+  if type(x) is int or ndigits is None: # the same as built-in round()
+    return int(y)
+  else:
+    return y
+  
+
+if __name__ == '__main__':
+  assert round(2.5) == 3 # built-in round() returns 2
+  assert round(3.5) == 4 # built-in round() returns 4
